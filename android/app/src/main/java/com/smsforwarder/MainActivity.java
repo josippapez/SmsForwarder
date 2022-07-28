a0b9c973b93bf82d0fb29c99ab6f91f849d0a7a1
Josip <papezjosip@gmail.com>
2022-07-28 16:26:32 +0200

    Main
    upgraded react native and react versions, also updated some packages

--- android/app/src/main/java/com/smsforwarder/MainActivity.java
+++ android/app/src/main/java/com/smsforwarder/MainActivity.java
@@ -29,6 +29,13 @@ public class MainActivity extends ReactActivity {
       super(activity, mainComponentName);
     }
 
+     @Override
+    protected boolean isConcurrentRootEnabled() {
+      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+      // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
+
     @Override
     protected ReactRootView createRootView() {
       ReactRootView reactRootView = new ReactRootView(getContext());
