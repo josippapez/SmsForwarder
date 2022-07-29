97f55547d5e2292fd9b449fb0ae8abb8b1bf6f74
Josip <papezjosip@gmail.com>
2022-07-29 11:56:38 +0200

    privacy policy added

--- store/reducers/reducer.ts
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from '@reduxjs/toolkit';
-import settings from './settings';
-
-export const reducers = combineReducers({settings});
-
-export type RootState = ReturnType<typeof reducers>;
